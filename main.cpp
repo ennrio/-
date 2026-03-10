164f48f2315166a99e849338c850fe23e2510a99
ennrio <Egor89532757638@yandex.ru>
2026-03-11 01:29:34 +0300

    хочу квадратик

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ void debugHandler(QtMsgType type, const QMessageLogContext &context, const QStri
 int main(int argc, char *argv[])
 {
     qInstallMessageHandler(debugHandler);
-    qDebug() << "fjdsdfafads";
+    qDebug() << "green";
     QApplication a(argc, argv);
     setDarkPalette(a);
     SimulationView view;
